7792bb1229eeb8eca03a71bf6f49856b6e2124dd
Swaggadan <dtaitt1@hotmail.com>
2020-11-20 09:18:25 -0400

    Add logic to make json outputs. Add classes

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import json
 import re
 import math
+import os
+
+class blockdata:
+    def __init__(self):
+        self.blockNumber = addressData()
+
+class addressData:
+    def __init__(self):
+        self.address = mnData()
+
+class mnData:
+    def __init__(self, collat, amt):
+        self.collateral = collat
+        self.amount = amt
+
 
 
 def read_data():
     output = {}
-    with open('listmn_38412.json', 'r') as file:
-        data = file.read()
-        obj = json.loads(data)
-        fullName = re.split('_', file.name)
-        blockNumber = fullName[1].split(".json")
-        for i in obj:
-            # output[blockNumber] = json.dumps()
-            # output["collateral"] = i["
-            print(i["owner"])
-            convert(i["collateral"])
-            
+    outer = {}
+    inner = {}
+    for dirpath, dirs, files in os.walk("."):
+        for filename in files:
+            if filename.endswith(".json"):
+                with open(filename, 'r') as file:
+                    data = file.read()
+                    obj = json.loads(data)
+                    fullName = re.split('_', file.name)
+                    blockNumber = fullName[1].split(".json")
+                    for i in obj:
+                        inner["collateral"] = convert(i["collateral"])
+                        inner["amount"] = 50
+                        outer[i["owner"]] = json.dumps(inner)
+                        output[blockNumber[0]] = json.dumps(outer)
+
+    print(output)
 
 
 def convert(num):
     p = (f'{num:f}')
     print(p)
+    return p
 
 
 
